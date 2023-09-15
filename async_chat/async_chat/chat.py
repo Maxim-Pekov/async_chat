6e3ae8c2b70cde75ee7f0eea1bb8e9fbdee532e6
Maxim-Pekov <max.pekov@gmail.com>
2023-09-16 00:08:51 +0300

    Add CLI options.

--- async_chat/async_chat/chat.py
+++ async_chat/async_chat/chat.py
@@ -1,28 +1,52 @@
 import asyncio
+import pathlib
+
 import aiofiles
+import configargparse
+import contextvars
 
 from datetime import datetime
 
 
-async def write_into_file(data):
-    async with aiofiles.open('x.txt', mode='a') as f:
-        chat_text = f"{datetime.now().strftime('[%d.%m.%Y %I:%M]')} {data}"
+def create_parser():
+    parser = configargparse.ArgParser(default_config_files=['.env'])
+
+    parser.add('-f', '--FILE', env_var='FILE', help='файл назначения для логов', default='history', type=str, nargs='?')
+    parser.add('-c', '--HOST', env_var='HOST', help='хост секретного чата', default='minechat.dvmn.org', type=str, nargs='?')
+    parser.add('-p', '--PORT', env_var='PORT', help='порт секретного чата', default=5000, type=int, nargs='?')
+    return parser
+
+
+async def write_into_file(chat_phrase):
+    # pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+    async with aiofiles.open(connections_vars.get().FILE, mode='a') as f:
+        chat_text = f"{datetime.now().strftime('[%d.%m.%Y %I:%M]')} {chat_phrase}"
         await f.write(chat_text)
         print(chat_text)
 
 
 async def read_from_socket(reader):
     while True:
-        data = await reader.read(512)
-        if not data:
+        chat_phrase = await reader.read(512)
+        if not chat_phrase:
             break
-        await write_into_file(data.decode("utf-8"))
+        await write_into_file(chat_phrase.decode("utf-8"))
 
 
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        connections_vars.get().HOST, connections_vars.get().PORT)
     await read_from_socket(reader)
 
 
-asyncio.run(tcp_echo_client())
+def main():
+    parser = create_parser()
+    options = parser.parse_args()
+
+    connections_vars.set(options)
+    asyncio.run(tcp_echo_client())
+
+
+if __name__ == "__main__":
+    connections_vars = contextvars.ContextVar('host')
+    main()
