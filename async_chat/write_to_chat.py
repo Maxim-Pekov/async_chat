08d45684d939d688ed2870f577e16b74ec9521fc
Maxim-Pekov <max.pekov@gmail.com>
2023-09-17 11:33:39 +0300

    Add check for broken token.

--- async_chat/write_to_chat.py
+++ async_chat/write_to_chat.py
@@ -5,9 +5,20 @@ import aiofiles
 import configargparse
 import contextvars
 import logging
+import json
 
 from datetime import datetime
 
+
+async def check_token(reader, writer, token):
+    writer.write(token.encode())
+    await writer.drain()
+    data = await reader.read(100)
+    logging.info(f'Received: {data.decode()!r}')
+    if json.loads(data.decode().split('\n')[0]) is None:
+        logging.info('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        return False
+    return True
 
 async def tcp_echo_client(message=""):
     reader, writer = await asyncio.open_connection(
@@ -16,23 +27,27 @@ async def tcp_echo_client(message=""):
     data = await reader.read(100)
     logging.info(f'Received: {data.decode()!r}')
 
-    print(f'Send: {message!r}')
-    writer.write("dfaab544-5414-11ee-aae7-0242ac110002\n".encode())
+    is_token_approve = await check_token(reader, writer, "dfaab544-5414-11ee-aae7-0242ac110002_\n")
+
+    if not is_token_approve:
+        logging.info(f'Введите ваше имя')
+        name = input()
+        writer.write(f"{name}\n".encode())
+        await writer.drain()
+
+    data = await reader.read(100)
+    logging.info(f'5Received: {data.decode()!r}')
+
+
+    writer.write("Всем привет!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n".encode())
     await writer.drain()
 
     data = await reader.read(100)
-    logging.info(f'Received: {data.decode()!r}')
+    logging.info(f'6Received: {data.decode()!r}')
 
 
-    writer.write("Всем привет!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n".encode())
-    await writer.drain()
-
-    data = await reader.read(100)
-    logging.info(f'Received: {data.decode()!r}')
-
-
-    writer.write("\n".encode())
-    await writer.drain()
+    # writer.write("\n".encode())
+    # await writer.drain()
 
     data = await reader.read(100)
     logging.info(f'Received: {data.decode()!r}')
@@ -45,13 +60,13 @@ async def tcp_echo_client(message=""):
 
 def main():
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format='%(name)s - %(levelname)s - %(message)s',
         datefmt='%d-%m-%Y %I:%M:%S %p',
-        level=logging.CRITICAL
+        level=logging.INFO
     )
     asyncio.run(tcp_echo_client())
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__file__)
+    logger = logging.getLogger('writer')
     main()
